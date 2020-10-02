d651c35bdf8d59d0af4838bed9090c8360aedb54
ericlesn <ericlesnunes1@outlook.com>
2020-10-01 23:25:20 -0300

    parabens

---
+++ WeatherApp/WeatherApp/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "475e7115f782adcda1bfc669f8a09568";
+    }
+}
